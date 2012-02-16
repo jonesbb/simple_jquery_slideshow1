2aff09cc1e7a1acac214de62a7ddfe3c78b871af
jonesbb <jonesbb@gmail.com>
2012-02-15 18:38:51 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SimpleJquerySlideshow1::Application.config.secret_token = '71ebf1b5ddaf9b1324b6b1124af52f3710a82a2683fe66d1298c8e0c72d0c0eae704ee71353ba154f32b5c4c42257669f013c15780cc181b072bd24c77034656'
