b9314708952f5738938bf44471222b2fdab1bcdd
jonesbb <jonesbb@gmail.com>
2012-02-15 19:26:14 -0800

    removed def index and added simple jquery slideshow

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 SimpleJquerySlideshow1::Application.routes.draw do
+
+  match '/contact', :to => 'pages#contact'
+  match '/about',   :to => 'pages#about'
+  root :to => 'pages#home'
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,25 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
+//Code from http://jonraasch.com/blog/a-simple-jquery-slideshow
+
+function slideSwitch() {
+    var $active = $('#slideshow IMG.active');
+
+    if ( $active.length == 0 ) $active = $('#slideshow IMG:last');
+
+    var $next =  $active.next().length ? $active.next()
+        : $('#slideshow IMG:first');
+
+    $active.addClass('last-active');
+
+    $next.css({opacity: 0.0})
+        .addClass('active')
+        .animate({opacity: 1.0}, 1000, function() {
+            $active.removeClass('active last-active');
+        });
+}
+
+$(function() {
+    setInterval( "slideSwitch()", 5000 );
+});
